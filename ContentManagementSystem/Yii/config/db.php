bbdf66888e4fc8dbf03e423a841a753a1941e7d6
croatiangrn <croatiangrn@gmail.com>
2015-07-10 15:37:11 +0200

    Added db file

---
+++ ContentManagementSystem/Yii/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=demo_cms',
+    'username' => 'demo',
+    'password' => 'demo123',
+    'charset' => 'utf8',
+];
