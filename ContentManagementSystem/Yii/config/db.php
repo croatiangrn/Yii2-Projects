8b95e8fda33143f949db0fc32058121aa76ec946
croatiangrn <croatiangrn@gmail.com>
2015-07-10 15:06:05 +0200

    Update in Yii/config/

--- ContentManagementSystem/Yii/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=demo_cms',
-    'username' => 'demo',
-    'password' => 'demo123',
-    'charset' => 'utf8',
-];
